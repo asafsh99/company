d538dc6169ba6ad655b1c7d962f33861aa9c2df6
asaf shaham <asafsh@gmail.com>
2024-12-17 11:02:48 +0200

    validate func update to None oviase

--- 2025/assignment/part_2/data_utils.py
+++ 2025/assignment/part_2/data_utils.py
@@ -1,23 +1,19 @@
 # check phone number
 def validate_phone_number(number):
     if number is None:
-        return None
+        return
 
     if len(number) == 11 and number[:2] == "05" and number[2].isdigit() and number[3] == "-" and number[4:].isdigit():
         return number
-    else:
-        return None
 
 
 # check gender
 def validate_gender(gender):
     if gender is None:
-        return None
+        return
 
     if gender == 'F' or gender == 'M':
         return gender
-    else:
-        return None
 
-# check years_experience
+
 
