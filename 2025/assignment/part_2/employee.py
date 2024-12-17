606aea5e0cbcb2857553d3cf3103e35ada9e404b
asaf shaham <asafsh@gmail.com>
2024-12-17 11:04:14 +0200

    update eq and hash, no deapartmeant

--- 2025/assignment/part_2/employee.py
+++ 2025/assignment/part_2/employee.py
@@ -2,11 +2,11 @@ import person as p
 
 
 class Employee(p.Person):
-    def __init__(self, e_id:int, firstname:str, lastname:str, address, phone_number:int, gender:str, salary:int, seniority:int):
+    def __init__(self, e_id: int, firstname: str, lastname: str, address, phone_number: int, gender: str, salary: int,
+                 seniority: int):
         super().__init__(e_id, firstname, lastname, address, phone_number, gender)
         self.__salary = salary
         self.__seniority = seniority
-
 
     def __str__(self):
         return f'{super().__str__()},{self.__salary},{self.__seniority}'
@@ -18,11 +18,10 @@ class Employee(p.Person):
         return (super().__eq__(other)
                 and isinstance(other, Employee)
                 and self.__salary == other.__salary
-                and self.__seniority == other.__seniority
-                and self.__department == other.__department)
+                and self.__seniority == other.__seniority)
 
     def __hash__(self):
-        return hash((super().__hash__(), self.__salary, self.__seniority, self.__department))
+        return hash((super().__hash__(), self.__salary, self.__seniority))
 
     def to_csv(self):
         return super().to_csv() + [self.__salary, self.__seniority]
